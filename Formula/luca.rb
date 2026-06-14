dec54da882ed68144421c8da2f5184dc8d09a5e3
Takuto NAKAMURA (Kyome) <kyomesuke@gmail.com>
2026-06-15 02:04:25 +0900

    Update luca.rb

--- Formula/luca.rb
+++ Formula/luca.rb
@@ -1,15 +1,10 @@
 class Luca < Formula
   desc "Command line tool to construct LUCA for Xcode project."
   homepage "https://github.com/Kyome22/LUCA"
-  url "https://github.com/Kyome22/LUCA/archive/refs/tags/2.1.0.tar.gz"
-  sha256 "3fe48263d591f8568f389d63ad5b07cbfbf90ea4ef485ee5f3eab4990a555300"
+  url "https://github.com/Kyome22/LUCA/archive/refs/tags/2.2.0.tar.gz"
+  sha256 "0360df1fe4fdc27fbec00918e7c8a7d3570c2d4e0c9bc3eeffa00738bc3f2450"
   license "MIT"
   head "https://github.com/Kyome22/LUCA.git", branch: "main"
-
-  bottle do
-    root_url "https://github.com/Kyome22/LUCA/releases/download/2.1.0"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe: "fa705682582b14ca893ecb1076594ca49b477b379f143a6e932392cbe2de332b"
-  end
 
   depends_on xcode: ["26.0", :build]
 
